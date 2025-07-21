b31a004f7f9bceb07867c0a8e8b6c72a1fb436ab
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-21 16:48:48 +0600

    consume the order and emit to client

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,6 +1,6 @@
 import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
 import { MessageBroker } from "../types/broker";
-
+import ws from "../socket";
 export class KafkaBroker implements MessageBroker {
   private consumer: Consumer;
 
@@ -39,6 +39,17 @@ export class KafkaBroker implements MessageBroker {
           topic,
           partition,
         });
+
+        switch (topic) {
+          case "order": {
+            // todo: maybe check event_type ?
+            const order = JSON.parse(message.value.toString());
+            ws.io.to(order.data.tenantId).emit("order-update", order);
+          }
+          break;
+          default: 
+            console.log("Doing nothing....")
+        }
       },
     });
   }
